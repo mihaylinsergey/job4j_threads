6ab33b1b32e22a8537aa9df9e18aec9f3998b942
mihaylinsergey <semi999@mail.ru>
2022-04-18 22:27:08 +0300

    4. Скачивание файла с ограничением. [#144271 #289945]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -1,6 +1,7 @@
 package ru.job4j.thread;
 
 import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 
@@ -15,18 +16,22 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        try (BufferedInputStream in = new BufferedInputStream(new URL(this.url).openStream())) {
+        try (BufferedInputStream in = new BufferedInputStream(new URL(this.url).openStream());
+             FileOutputStream fileOutputStream = new FileOutputStream("10Mb.dat")) {
             byte[] dataBuffer = new byte[1024];
-            int bytesRead = 0;
-            while ((bytesRead != -1)) {
-                long start = System.nanoTime();
-                bytesRead = in.read(dataBuffer, 0, 1024);
-                long finish = System.nanoTime();
-                System.out.println(bytesRead);
-                long delta = finish - start;
-                long currentSpeed = finish - start;
-                if (currentSpeed < this.speed) {
-                    Thread.sleep((currentSpeed - (long) this.speed));
+            int bytesRead;
+            int downloadData = 0;
+            long start = System.nanoTime();
+            while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData += bytesRead;
+                fileOutputStream.write(dataBuffer, 0, bytesRead);
+                if (downloadData >= speed) {
+                    long finish = System.nanoTime();
+                    long delta = finish - start;
+                    if (delta < 1000000000) {
+                        Thread.sleep((1000 - delta / 1000000));
+                        downloadData = 0;
+                    }
                 }
             }
         } catch (IOException | InterruptedException e) {
@@ -37,6 +42,9 @@ public class Wget implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
+        if (url.isEmpty() || speed <= 0) {
+            throw new IllegalArgumentException("Argument is not correct");
+        }
         Thread wget = new Thread(new Wget(url, speed));
         wget.start();
         wget.join();
