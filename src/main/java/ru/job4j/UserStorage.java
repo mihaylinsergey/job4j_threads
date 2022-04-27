68af8d4a1f19120a2ee28b95eab0f0398ea82651
mihaylinsergey <semi999@mail.ru>
2022-04-27 21:12:44 +0300

    3. Класс хранилища пользователей UserStorage [#1104 #294040]

--- src/main/java/ru/job4j/UserStorage.java
+++ src/main/java/ru/job4j/UserStorage.java
@@ -10,53 +10,36 @@ import java.util.Map;
 public class UserStorage {
 
     @GuardedBy("this")
-    private static final Map<Integer, User> STORES = new HashMap<>();
+    private final Map<Integer, User> stores = new HashMap<>();
 
     public synchronized boolean add(User user) {
-        boolean rsl = false;
-        if (user != null) {
-            STORES.putIfAbsent(user.getId(), user);
-            rsl = true;
-        }
-        return rsl;
+        return stores.putIfAbsent(user.getId(), user) == null;
     }
 
     public synchronized boolean update(User user) {
-        boolean rsl = false;
-        if (user != null) {
-            STORES.replace(user.getId(), user);
-            rsl = true;
-        }
-        return rsl;
+        return stores.replace(user.getId(), user) == null;
     }
 
     public synchronized boolean delete(User user) {
-        boolean rsl = false;
-        if (user != null) {
-            STORES.remove(user.getId());
-            rsl = true;
-        }
-        return rsl;
+        return stores.remove(user.getId()) == null;
     }
 
     public synchronized void transfer(int fromId, int toId, int amount) {
-        if (STORES.get(fromId).getAmount() < 0) {
-            throw new IllegalArgumentException("Not enough funds");
+        User from = stores.get(fromId);
+        User to = stores.get(toId);
+        if (from == null || to == null || from.getAmount() < 0) {
+            throw new IllegalArgumentException("Not enough funds or wrong users");
         }
-        User from = STORES.get(fromId);
         from.setAmount(from.getAmount() - amount);
-        User to = STORES.get(toId);
         to.setAmount(to.getAmount() + amount);
-        update(from);
-        update(to);
     }
 
     public static void main(String[] args) {
         UserStorage store = new UserStorage();
         store.add(new User(1, 100));
         store.add(new User(2, 200));
-        STORES.forEach((key, value) -> System.out.println(key + " : " + value.getAmount()));
+        store.stores.forEach((key, value) -> System.out.println(key + " : " + value.getAmount()));
         store.transfer(1, 2, 50);
-        STORES.forEach((key, value) -> System.out.println(key + " : " + value.getAmount()));
+        store.stores.forEach((key, value) -> System.out.println(key + " : " + value.getAmount()));
     }
 }
