665f06fcfd25fab955a89649a127eaa3c2454703
mihaylinsergey <semi999@mail.ru>
2022-04-28 20:37:55 +0300

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -0,0 +1,54 @@
+package ru.job4j;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        CountBarrier barrier = new CountBarrier(50000000);
+        Thread master = new Thread(
+                () -> {
+                    System.out.println(Thread.currentThread().getName() + " started");
+                    while (true) {
+                        barrier.count();
+                    }
+                },
+                "Master"
+        );
+        Thread slave = new Thread(
+                () -> {
+                    barrier.await();
+                    System.out.println(Thread.currentThread().getName() + " started");
+                },
+                "Slave"
+        );
+        master.start();
+        slave.start();
+    }
+}
